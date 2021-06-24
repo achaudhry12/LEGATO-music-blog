90a88d4aea60ed289d890d1c7fd768c4975f36d1
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-06-24 14:52:48 -0400

    services folder front end basecode

---
+++ client/src/services/songs.js
@@ -0,0 +1,31 @@
+import api from './api-config';
+
+export const getAllFoods = async () => {
+	const resp = await api.get('/foods');
+	return resp.data;
+};
+
+export const getOneFood = async (id) => {
+	const resp = await api.get(`/foods/${id}`);
+	return resp.data;
+};
+
+export const postFood = async (foodData) => {
+	const resp = await api.post('/foods', { food: foodData });
+	return resp.data;
+};
+
+export const putFood = async (id, foodData) => {
+	const resp = await api.put(`/foods/${id}`, { food: foodData });
+	return resp.data;
+};
+
+export const deleteFood = async (id) => {
+	const resp = await api.delete(`/foods/${id}`);
+	return resp;
+};
+
+export const addFlavor = async (foodId, flavorId) => {
+	const resp = await api.put(`/flavors/${flavorId}/foods/${foodId}`);
+	return resp.data;
+};
