3a32238dbab1db6f3abdc4fbcc1be52b6d67f088
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-06-28 13:38:17 -0400

    changed Artists

--- client/src/screens/Artists.jsx
+++ client/src/screens/Artists.jsx
@@ -8,14 +8,17 @@ export default function Artists(props) {
       <h1>ARTISTS</h1>
       {artists.map((artists) => (
         <div>
+          <button>
           <img key={artists.id} src={artists.image_url} alt={artists.stage_name}></img>
           <br />
-          <button>
             <h2 key={artists.id}>{artists.stage_name}</h2>
             <p key={artists.id}>{artists.full_name}</p>
           </button>
         </div>
       ))}
+      <button>
+        ADD ARTIST
+      </button>
 		</div>
 	);
 }
