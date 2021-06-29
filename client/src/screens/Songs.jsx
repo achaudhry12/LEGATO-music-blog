bf632af5089237a734654047a5fca2d6e0a1b0d3
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-06-29 10:42:06 -0400

    extra stuff

--- client/src/screens/Songs.jsx
+++ client/src/screens/Songs.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getOneArtist } from "../services/artists";
-import "../assets/css/Songs.css"
+import "../assets/css/Songs.css";
 
 export default function Songs(props) {
   // const { songs } = props;
-  const [ artists, setArtists ] = useState([null]);
+  const [artists, setArtists] = useState([null]);
   const { id } = useParams();
-  
+
   useEffect(() => {
     const fetchArtists = async () => {
       const artistInfo = await getOneArtist(id);
@@ -20,23 +20,34 @@ export default function Songs(props) {
 
   return (
     <>
-    <br />
-    <div className="song-body">
-        <h1 class='title'>SONGS</h1>
-      <Link to={`/artists/${artists.id}/songs/create`}>
-        <button className="add-button">ADD SONG</button>
-      </Link>
       <br />
-      <br />
-      <br />
-      <div>
-      {artists?.songs?.map((song) => (
-        <div className="song-container">
-          <p key={song.id}>{song.name}</p>
-        </div>
-        ))}
+      <div className="song-body">
+        <h1 class="title">SONGS</h1>
+        <Link to={`/artists/${artists.id}/songs/create`}>
+          <button className="add-button">ADD SONG</button>
+        </Link>
+        <br />
+        <br />
+        <br />
+        <div>
+          {artists?.songs?.map((song) => (
+            <div className="song-container">
+              <button class="link">
+              <a  href={song.link}>
+                <p key={song.id}>{song.name}</p>
+                <p key={song.id}>{song.release_date}</p>
+                </a>
+                </button>
+            </div>
+          ))}
         </div>
       </div>
-      </>
+      <br />
+      <br />
+      <br />
+      <br />
+        <br />
+        <br />
+    </>
   );
 }
